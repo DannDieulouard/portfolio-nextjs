c1308bbc2336c874588dd004219889f3cc017b0f
DannDieulouard <dann.dieulouard@lapiscine.pro>
2024-09-06 18:06:01 +0200

    changing navigator title

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <section className='pb-24 pt-40'>
       <div className='container max-w-3xl'>
-        <h2 className='title'>We should keep in touch !</h2>
+        <h2 className='title'>We should keep in touch.</h2>
 
         <ContactForm />
       </div>
