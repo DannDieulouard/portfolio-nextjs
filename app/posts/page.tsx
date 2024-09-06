0f72480e68cbce6438b4638e9967bab43c391008
DannDieulouard <dann.dieulouard@lapiscine.pro>
2024-09-06 14:58:52 +0200

    add all posts page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import { getPosts } from '@/lib/posts'
+// import PostsWithSearch from '@/components/posts-with-search'
+import Posts from '@/components/posts'
 
-export default function PostsPage() {
-    return <div>PostsPage</div>
+export default async function PostsPage() {
+  const posts = await getPosts()
+
+  return (
+    <section className='pb-24 pt-40'>
+      <div className='container max-w-3xl'>
+        <h1 className='title mb-12'>Posts</h1>
+
+        <Posts posts={posts} />
+      </div>
+    </section>
+  )
 }
